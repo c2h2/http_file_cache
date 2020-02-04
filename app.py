0364cf79e150e863ffba23b73f738dd5db39056a
Yiling Cao <yiling.cao@gmail.com>
2020-02-04 18:13:29 +0800

    add twitter profile/background image cache.

--- app.py
+++ app.py
@@ -1,39 +1,90 @@
-from quart import Quart, request, send_from_directory
+from quart import Quart, request, send_from_directory, redirect
 import json
 import base58
 import os
 import urllib
+import twint
+import subprocess
+import time
 
 app = Quart(__name__)
+
+STORE_LOC="stores"
+DEFAULT_EXT="jpg"
+RETRY_TIMES=2
 
 # load conf
 with open('./config.json') as f:
     config = json.load(f)
 app.config.update(config)
 
-@app.route('/')
-async def index():
-    return 'Hello World'
 
-@app.route('/file')
-async def serve():
-    key = request.args.get('key')
-    url = request.args.get('url')
+def key_url_to_file(key, url):
+    #ensure url safe.
     enc_key=str(base58.b58encode(key), 'utf-8')
     enc_url=str(base58.b58encode(url), 'utf-8')
 
     ext = url.split(".")[-1]
     if len(ext)>5:
-        ext = "unkonwn"
+        ext = DEFAULT_EXT
     fn = enc_key+"_"+enc_url+"."+ext
-    path_fn = "stores/"+fn
+    path_fn = STORE_LOC+"/"+fn
     print(path_fn)
     if os.path.exists(path_fn):
         print("SERVE FROM FILE")
     else:
         urllib.request.urlretrieve(url, path_fn)
-    return await send_from_directory('stores', fn)
 
+    return path_fn, fn
+
+def get_twitter_lookup_json(username):
+    path_fn = STORE_LOC+"/"+username+"_lookup.json"
+    cmd = "twint --user-full --json -u " + username + " -o " + path_fn
+    if os.path.exists(path_fn):
+        print("JSON FROM FILE")
+    else:
+        #os.remove(path_fn)
+        for attempt in range(RETRY_TIMES):
+            try:
+                subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)           
+            except subprocess.CalledProcessError as e:
+                print("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+                time.sleep(attempt*10) # longer and longer sleep
+                continue
+            break
+    return path_fn
+
+
+@app.route('/')
+async def index():
+    return 'Hello World, File Cache Server.'
+
+@app.route('/file')
+async def serve():
+    path_fn, fn = key_url_to_file(request.args.get('key'), request.args.get('url'))
+    return await send_from_directory(STORE_LOC, fn)
+
+@app.route('/twitter_profile_image/<path>')
+async def twitter_profile_image(path):
+    path_fn = get_twitter_lookup_json(path)
+
+    with open(path_fn) as json_file:
+        data = json.load(json_file)
+        url = data["profile_image_url"]
+    
+    path_fn, fn = key_url_to_file("twitter_profile_image", url)
+    return await send_from_directory(STORE_LOC, fn)
+
+@app.route('/twitter_background_image/<path>')
+async def twitter_background_image(path):
+    path_fn = get_twitter_lookup_json(path)
+
+    with open(path_fn) as json_file:
+        data = json.load(json_file)
+        url = data["background_image"]
+    
+    path_fn, fn = key_url_to_file("twitter_background_image", url)
+    return await send_from_directory(STORE_LOC, fn) 
 
 
 if __name__ == '__main__':
